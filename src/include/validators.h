e728c0bae78b2637b2443b9e7e4631b84019ab71
santerijps <santeri.sydanmetsa@pm.me>
2022-06-21 13:08:43 +0300

    fix(gcc): remove inline

--- src/include/validators.h
+++ src/include/validators.h
@@ -1,8 +1,8 @@
-inline int char_is_alpha(char c);
-inline int char_is_digit(char c);
-inline int char_is_whitespace(char c);
-inline int char_is_punctuation(char c);
-inline int char_is_operator(char c);
-inline int char_is_symbol(char c);
-inline int char_is_word(char c);
-inline int char_is_number(char c);
+int char_is_alpha(char c);
+int char_is_digit(char c);
+int char_is_whitespace(char c);
+int char_is_punctuation(char c);
+int char_is_operator(char c);
+int char_is_symbol(char c);
+int char_is_word(char c);
+int char_is_number(char c);
--- src/validators.c
+++ src/validators.c
@@ -1,34 +1,34 @@
 #include "include/validators.h"
 
-inline int char_is_alpha(char c) {
+int char_is_alpha(char c) {
   return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
-inline int char_is_digit(char c) {
+int char_is_digit(char c) {
   return c >= '0' && c <= '9';
 }
 
-inline int char_is_whitespace(char c) {
+int char_is_whitespace(char c) {
   return c == ' ' || c == '\t' || c == '\n';
 }
 
-inline int char_is_punctuation(char c) {
+int char_is_punctuation(char c) {
   return
     c == ',' || c == '.' || c == ';' ||
     c == ':' || c == '?' || c == '!';
 }
 
-inline int char_is_operator(char c) {
+int char_is_operator(char c) {
   return
     c == '+' || c == '-' || c == '*' || c == '/' ||
     c == '%' || c == '^' || c == '|' || c == '&';
 }
 
-inline int char_is_symbol(char c) {
+int char_is_symbol(char c) {
   return c == '$' || c == '@' || c == '#';
 }
 
-inline int char_is_word(char c) {
+int char_is_word(char c) {
   return
     char_is_alpha(c)
     || char_is_digit(c)
@@ -36,6 +36,6 @@ inline int char_is_word(char c) {
     || c == '-';
 }
 
-inline int char_is_number(char c) {
+int char_is_number(char c) {
   return char_is_digit(c) || c == '_' || c == '.';
 }
