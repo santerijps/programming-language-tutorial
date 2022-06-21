4f8e9ba2a388d1416b10d1b2756fd6e495ed9b63
santerijps <santeri.sydanmetsa@pm.me>
2022-06-21 20:38:52 +0300

    test: update main with token iter

--- src/pltc.c
+++ src/pltc.c
@@ -5,6 +5,8 @@
 int main(int argc, char **argv) {
 
   LEX_RESULT lex_result;
+  TOKEN_ITER token_iter;
+  TOKEN token;
 
   if (argc == 1) {
     fprintf(stderr, "Please provide a source file!\n");
@@ -16,9 +18,16 @@ int main(int argc, char **argv) {
   printf("Exit code: %d, token count: %d\n",
     lex_result.exit_code, lex_result.token_count);
 
-  for (int i = 0; i < lex_result.token_count; i++) {
-    token_print(lex_result.token_list[i]);
-    free(lex_result.token_list[i].value);
+  if (lex_result.exit_code != 0) {
+    exit(1);
+  }
+
+  token_iter = token_iter_new(lex_result.token_count, lex_result.token_list);
+
+  while (token_iter_has_next(&token_iter)) {
+    token = token_iter_next(&token_iter);
+    token_print(token);
+    token_destroy(&token);
   }
 
 }
