68505294221a4fef791b8fbbd7fdfa3cde9260b3
santerijps <santeri.sydanmetsa@pm.me>
2022-06-21 13:12:07 +0300

    fix(validators): add macro definition

--- src/include/validators.h
+++ src/include/validators.h
@@ -1,8 +1,14 @@
-int char_is_alpha(char c);
-int char_is_digit(char c);
-int char_is_whitespace(char c);
-int char_is_punctuation(char c);
-int char_is_operator(char c);
-int char_is_symbol(char c);
-int char_is_word(char c);
-int char_is_number(char c);
+#ifndef VALIDATORS_H
+
+  #define VALIDATORS_H
+
+  int char_is_alpha(char c);
+  int char_is_digit(char c);
+  int char_is_whitespace(char c);
+  int char_is_punctuation(char c);
+  int char_is_operator(char c);
+  int char_is_symbol(char c);
+  int char_is_word(char c);
+  int char_is_number(char c);
+
+#endif
